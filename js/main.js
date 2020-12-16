be8a4e6a22d8dcd785255ce732687c09d908a514
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-16 17:59:23 +0100

    Chiamata al server e collegamento Vue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// Generare 10 email e stamparle a schermo, utilizzando Vue, Axios e l'endpoint relativo nelle slide che sto per aggiungere su Drive.
+// Generare 10 email e stamparle a schermo, utilizzando Vue, Axios e l'endpoint relativo nelle slide.
+
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then( function (result) {
+    console.log(result);
+    console.log(result.data.response);
+  });
+
+var app = new Vue({
+  el: "#root",
+  data: {
+    randomEmail: "valore iniziale",
+  },
+  methods: {
+
+  }
+});
