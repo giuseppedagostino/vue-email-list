dbb827a36d1fc8e6c768674d0e89289b2910ef24
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-16 18:33:42 +0100

    Stampo 10 email ma sono tutte uguali e cambiano contemporaneamente

--- js/main.js
+++ js/main.js
@@ -3,16 +3,23 @@
 var app = new Vue({
   el: "#root",
   data: {
-    randomEmail: "valore iniziale",
+    randomEmail: "",
+    arrayEmail: []
   },
   methods: {},
   mounted: function() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then( (result) => {
-        console.log(result);
-        console.log(result.data.response);
-        this.randomEmail = result.data.response;
-      });
+    for (var i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then( (result) => {
+          console.log(result);
+          console.log(result.data.response);
+          // inserisco nel dato randomEmail il valore restituito dal server
+          this.randomEmail = result.data.response;
+          // pusho nell'array la singola email
+          this.arrayEmail.push(this.randomEmail);
+        });
+    }
+
   }
 });
