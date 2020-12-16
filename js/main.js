64766ebab5712fee0627ad91d698ed7493e7a698
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-16 18:09:29 +0100

    Email casuale stampata a schermo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // Generare 10 email e stamparle a schermo, utilizzando Vue, Axios e l'endpoint relativo nelle slide.
 
-axios
-  .get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then( function (result) {
-    console.log(result);
-    console.log(result.data.response);
-  });
-
 var app = new Vue({
   el: "#root",
   data: {
     randomEmail: "valore iniziale",
   },
-  methods: {
-
+  methods: {},
+  mounted: function() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then( (result) => {
+        console.log(result);
+        console.log(result.data.response);
+        this.randomEmail = result.data.response;
+      });
   }
 });
